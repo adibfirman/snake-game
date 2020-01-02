d84d71a84afb8c2894ec49a4889267728404b949
adibfirman <dib.firman@gmail.com>
2020-01-02 09:56:30 +0700

    add: static snake

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const canvas = document.querySelector("canvas");
+const w = window.innerWidth / 1.1;
+const h = window.innerHeight / 1.1;
 
-canvas.width = window.innerWidth / 1.1;
-canvas.height = window.innerHeight / 1.1;
+canvas.width = w;
+canvas.height = h;
 
 const c = canvas.getContext("2d");
 const primaryColor = "#6B6B11";
@@ -17,8 +19,17 @@ c.fill();
 c.stroke();
 
 /** Draw the snake */
-for (let i = 0; i < 2; i++) {
+const snakes = [{ x: 200, y: 200 }];
+const defaultLong = 5;
+for (let i = 1; i <= defaultLong; i++) {
+  const data = snakes[i - 1];
+
+  snakes.push({
+    x: data.x + 10,
+    y: data.y
+  });
+
   c.beginPath();
-  c.fillRect(110, 95, 20, 20);
+  c.fillRect(data.x, data.y, 16, 20);
   c.stroke();
 }
