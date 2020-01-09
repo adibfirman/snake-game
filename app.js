ce8dccd6a806d114c63da96d5e789e32de03ddb3
adibfirman <dib.firman@gmail.com>
2020-01-09 14:47:08 +0700

    add: remove rxjs

--- app.js
+++ app.js
@@ -1,80 +1,90 @@
 const canvas = document.querySelector("canvas");
-const w = Math.floor(window.innerWidth / 1.1);
-const h = Math.floor(window.innerHeight / 1.1);
+const w = Math.floor(window.innerWidth / 3);
+const h = Math.floor(window.innerHeight / 3.5);
 
 canvas.width = w;
 canvas.height = h;
 
+window.addEventListener("resize", () => {
+  canvas.width = w;
+  canvas.height = h;
+});
+
 const c = canvas.getContext("2d");
 const primaryColor = "#6B6B11";
-const baitWidth = 4;
 
 c.fillStyle = primaryColor;
 
 /** Draw bait */
-drawBait();
-function drawBait() {
+function drawBait(x, y) {
+  const r = 4;
+
   c.beginPath();
-  c.arc(100 + baitWidth, 100 + baitWidth, baitWidth, Math.PI * 2, false);
+  c.arc(x + r, y + r, 4, Math.PI * 2, false);
   c.strokeStyle = primaryColor;
   c.fill();
   c.stroke();
 }
 
-/** Draw the snake */
-const $snakes = new rxjs.BehaviorSubject([{ x: 100, y: 200 }]);
-const valSnakes = $snakes.getValue();
-const defaultLong = 5;
-
-/** create default snakes */
-for (let i = 0; i < defaultLong; i++) {
-  const data = valSnakes[i];
-  const x = data.x + 10;
-  const y = data.y;
-
-  valSnakes.push({ x, y });
-  $snakes.next(valSnakes);
+/** random number given range */
+function randomIntNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-/** adding move to right */
-const $moveToLeft = new rxjs.BehaviorSubject(false);
-$moveToLeft.subscribe(status => {
-  (function autoMove() {
-    return setInterval(() => {
-      const copyData = [...valSnakes];
-      const lastData = copyData[copyData.length - 1];
-
-      for (let i = 0; i < copyData.length; i++) {
-        if (!status) copyData[i].x += 10;
-        else copyData[i].x -= 10;
-      }
-
-      if (copyData[0].x < 0) clearInterval(autoMove);
-      else if (lastData.x + 10 > w) clearInterval(autoMove);
-      else {
-        c.clearRect(0, 0, w, h);
-        drawBait();
-
-        $snakes.next(copyData);
-      }
-    }, 1000);
-  })();
-});
-
-/** subs to draw a snake  */
-$snakes.subscribe(snakes => {
-  console.log(snakes);
-  for (let i = 0; i < snakes.length; i++) {
-    const data = snakes[i];
-
-    c.beginPath();
-    c.fillRect(data.x, data.y, 16, 20);
-    c.stroke();
+class Snake {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.velocity = {
+      x: 10,
+      y: 10
+    };
   }
-});
 
-/** adding keyboard event */
-window.addEventListener("keyup", e => {
-  if (e.keyCode === 37) $moveToLeft.next(true);
-  else if (e.keyCode === 39) $moveToLeft.next(false);
-});
+  draw() {
+    c.beginPath();
+    c.fillRect(this.x, this.y, this.w, this.h);
+    c.stroke();
+    c.closePath();
+  }
+
+  moveLeft() {
+    this.x += this.velocity.x;
+  }
+}
+
+const defaultLong = 5;
+const gap = 10;
+const snakes = [];
+(function init() {
+  const wSnake = 16;
+  const hSnake = 20;
+  let x = randomIntNum(wSnake, w);
+  let y = randomIntNum(hSnake, h);
+  for (let i = 0; i < defaultLong; i++) {
+    if (i !== 0) x += gap;
+
+    snakes.push(new Snake(x, y, wSnake, hSnake));
+  }
+})();
+
+setInterval(() => {
+  snakes.forEach(snake => snake.moveLeft());
+}, 1000);
+
+(function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, w, h);
+
+  drawBait(100, 100);
+  snakes.forEach(snake => {
+    snake.draw();
+
+    if (snake.x > w) snake.x = 0;
+    if (snake.x < 0) snake.x = w;
+    if (snake.y > h) snake.y = 0;
+    if (snake.y < 0) snake.y = h;
+  });
+})();
